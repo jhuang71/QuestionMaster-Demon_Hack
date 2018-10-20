dd88c4a6c2c62c200cb17da085c812a49fbf9481
Jay Huang <jhuang71@hawk.iit.edu>
2018-10-20 18:20:59 -0500

    updated some cool stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SigninComponent } from './signin/signin.component';
-import { NavComponent } from './nav/nav.component';
-import { NiceBackgroundComponent } from './nice-background/nice-background.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent,
-    NavComponent,
-    NiceBackgroundComponent
   ],
   imports: [
     BrowserModule
